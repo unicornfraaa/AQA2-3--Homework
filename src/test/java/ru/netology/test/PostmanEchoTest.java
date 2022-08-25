8dcc50e11b142a3e802bfcbbc8ae9513f0f264e4
Ksenia <ksuskaxD@gmail.com>
2022-08-25 12:46:36 +0300

    succesed test

--- src/test/java/ru/netology/test/PostmanEchoTest.java
+++ src/test/java/ru/netology/test/PostmanEchoTest.java
@@ -13,7 +13,7 @@ class postmanEcho {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("Hello")
+                .body("some data")
                 .when()
                 .post("/post")
                 .then()
