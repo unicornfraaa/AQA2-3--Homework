5a323076199e9371d36da81b3ccf8fa406039df8
Ksenia <ksuskaxD@gmail.com>
2022-08-25 12:43:59 +0300

    failed test

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology.test;
+
+import static io.restassured.RestAssured.authentication;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+
+class postmanEcho {
+
+    @Test
+    void shouldTestSomeData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Hello")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
